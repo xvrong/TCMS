cda66b6fd522af6b0f5ea55ff6081a6c01fe0a4a
Hou-Xiaoxuan <59465493+Hou-Xiaoxuan@users.noreply.github.com>
2021-07-06 20:25:33 +0800

    完成class类的框架构建

--- src/Class/Class.java
+++ src/Class/Class.java
@@ -1,4 +1,49 @@
 package Class;
 
 public abstract class Class { 
+    private ClassAttribute attribute; // 学历、年级、班级基本属性
+    private String classHeader; // 班主任
+    
+    public Class(){}
+    public Class(final String education, final int grade, final int classNumber, final String classHeader) {
+        this.attribute = new ClassAttribute(education, grade, classNumber);
+        this.classHeader = classHeader;
+    }
+    
+    public void showInfo() {
+        // TODO: Lx 展示教室的所有属性
+    }
+    
+    public String getClassHeader() {
+        return classHeader;
+    }
+    public void setClassHeader(String classHeader) {
+        this.classHeader = classHeader;
+    }
+
+    public String getEducation() {
+        return attribute.getEducation();
+    }
+
+
+    public void setEducation(final String education) {
+        attribute.setEducation(education);
+    }
+
+    public int getGrade() {
+        return attribute.getGrade();
+    }
+
+    public void setGrade(final int grade) {
+        attribute.setGrade(grade);
+    }
+
+    public int getClassNumber() {
+        return attribute.getClassNumber();
+    }
+
+    public void setClassNumber(final int classNumber) {
+        attribute.setClassNumber(classNumber);
+    }
+
 }
--- src/Class/PrimaryClass.java
+++ src/Class/PrimaryClass.java
@@ -2,4 +2,12 @@ package Class;
 
 public class PrimaryClass extends Class {
 
+    
+    public PrimaryClass() {
+    }
+
+    public PrimaryClass(String education, int grade, int classNumber, String classHeader) {
+        super(education, grade, classNumber, classHeader);
+    }
+
 }
