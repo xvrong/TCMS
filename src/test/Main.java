083d204bfce46373fad745f47c2eb4906eb35ae9
xvrong <1187518864@qq.com>
2021-07-14 13:52:05 +0800

    处理合并冲突，增加时间流逝模拟

--- src/test/Main.java
+++ src/test/Main.java
@@ -2,14 +2,21 @@
  * @Author: xv_rong
  * @Date: 2021-07-12 10:53:28
  * @LastEditors: xv_rong
- * @LastEditTime: 2021-07-14 10:36:42
+ * @LastEditTime: 2021-07-14 13:48:19
  * @Description: 
  * @FilePath: \TCMS\src\test\Main.java
  */
 package test;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Calendar;
+
 import SQL.Query;
 import SQL.Impl.QueryImpl;
+import dao.BaseDao;
 import serivice.ManagerService;
 import serivice.StudentService;
 import serivice.TeacherService;
@@ -21,6 +28,10 @@ import windows.impl.GetInputImpl;
 public class Main {
     static GetInputImpl gt = new GetInputImpl();
 
+    static {
+        checkYear();
+    }
+
     public static void main(String[] args) {
         System.out.println("-----------------课外辅导管理系统启动----------------");
         while (true) {
@@ -28,6 +39,7 @@ public class Main {
             System.out.println("1.学生登录");
             System.out.println("2.老师登录");
             System.out.println("3.管理员登录");
+            System.out.println("4.模拟增加年份");
             System.out.println("0.退出");
             int model = 0;
             model = gt.getInputInt("请输入序号: ", 0, 3);
@@ -40,6 +52,9 @@ public class Main {
                     break;
                 case 3:
                     managerLogin();
+                    break;
+                case 4:
+                    simulationAddYear();
                     break;
                 default:
                     return; // 程序退出
@@ -95,5 +110,76 @@ public class Main {
                 System.out.println("请重新输入");
             }
         } while (true);
+    }
+
+    static void updateSystem(int systemYear, int year) {
+        BaseDao bd = new BaseDao();
+        Object[] tmp = new Object[1];
+        tmp[0] = systemYear - year;
+        bd.executeSQL("update class SET grade = grade + ? WHERE state = TRUE", tmp);
+        bd.executeSQL("UPDATE class SET state=FALSE WHERE CASE education WHEN 0 THEN grade> 6 ELSE grade> 3 END", null);
+        bd.executeSQL(
+                "UPDATE taking SET state=FALSE WHERE taking.classID IN (SELECT classID FROM class WHERE state=FALSE)",
+                null);
+        bd.executeSQL(
+                "UPDATE class SET grade=CASE WHEN education=0 AND grade> 6 THEN 6 WHEN (education=1 OR education=2) AND grade> 3 THEN 3 ELSE grade END",
+                null);
+        tmp[0] = systemYear;
+        bd.executeSQL("insert into allyear VALUES(?)", tmp);
+    }
+
+    static void checkYear() {
+        BaseDao bd = new BaseDao();
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        int year = 0;
+        try {
+            // 数据库上一次更新的时间
+            String sql = "select max(yearId) from allyear where state = true";
+            conn = bd.getConn();
+            pstmt = conn.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                year = rs.getInt(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            bd.closeAll(conn, pstmt, rs);
+        }
+        Calendar c1 = Calendar.getInstance();
+        int systemYear = c1.get(Calendar.YEAR);
+        if (systemYear > year) {
+            updateSystem(systemYear, year);
+        }
+    }
+
+    static void simulationAddYear() {
+        BaseDao bd = new BaseDao();
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        int year = 0;
+        try {
+            String sql = "select max(yearId) from allyear where state = true";
+            conn = bd.getConn();
+            pstmt = conn.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                year = rs.getInt(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            bd.closeAll(conn, pstmt, rs);
+        }
+        updateSystem(year + 1, year);
     }
 }
--- src/windows/impl/PrintImpl.java
+++ src/windows/impl/PrintImpl.java
@@ -1,8 +1,8 @@
 /*
  * @Author: xv_rong
  * @Date: 2021-07 21:36:58
- * @LastEditors: LinXuan
- * @LastEditTime: 2021-07-14 12:09:00
+ * @LastEditors: xv_rong
+ * @LastEditTime: 2021-07-14 13:50:58
  * @Description: 
  * @FilePath: \TCMS\src\windows\impl\PrintImpl.java
  */
@@ -19,138 +19,127 @@ import entity.Person.Teacher;
 import entity.TClass.TClass;
 import windows.Print;
 
-// TODO:调整输出顺序
-// TODO: 输入密码隐藏
 public class PrintImpl implements Print {
-    private static PrintStream jout = System.out; // 全局的输出
+        private static PrintStream jout = System.out; // 全局的输出
 
-    @Override
-    public void printStudentBasicInfomation(ArrayList<Student> studentList) {
-        jout.println("学生的基本信息：");
-        jout.println("--------------------------------------------------------");
-        jout.printf("%-8s\t%-8s\t%-8s\n", "序号", "学生ID", "姓名");
-        jout.println("--------------------------------------------------------");
-        int number = 0;
-        for (Student stu : studentList) {
-            jout.printf("%-8d\t%-8s\t%-8s\n", number, stu.getStudentID(), stu.getName());
-            number++;
+        @Override
+        public void printStudentBasicInfomation(ArrayList<Student> studentList) {
+                jout.println("学生的基本信息：");
+                jout.println("--------------------------------------------------------");
+                jout.printf("%-8s\t%-8s\t%-8s\n", "序号", "学生ID", "姓名");
+                jout.println("--------------------------------------------------------");
+                int number = 0;
+                for (Student stu : studentList) {
+                        jout.printf("%-8d\t%-8s\t%-8s\n", number, stu.getStudentID(), stu.getName());
+                        number++;
+                }
+                jout.println("---------------------------------------------------------");
         }
-        jout.println("---------------------------------------------------------");
-    }
 
-    @Override
-    public void printTeacherBasicInfomation(ArrayList<Teacher> teacherList) {
-        jout.println("老师的基本信息：");
-        jout.println("--------------------------------------------------------------------------------");
-        jout.printf("%-8s\t%-8s\t%-8s\n", "序号", "老师ID", "姓名");
-        jout.println("--------------------------------------------------------------------------------");
-        int number = 0;
-        for (Teacher tch : teacherList) {
-            jout.printf("%-8d\t%-8s\t%-8s\n", number, tch.getTeacherID(), tch.getName());
-            number++;
+        @Override
+        public void printTeacherBasicInfomation(ArrayList<Teacher> teacherList) {
+                jout.println("老师的基本信息：");
+                jout.println("--------------------------------------------------------------------------------");
+                jout.printf("%-8s\t%-8s\t%-8s\n", "序号", "老师ID", "姓名");
+                jout.println("--------------------------------------------------------------------------------");
+                int number = 0;
+                for (Teacher tch : teacherList) {
+                        jout.printf("%-8d\t%-8s\t%-8s\n", number, tch.getTeacherID(), tch.getName());
+                        number++;
+                }
+                jout.println("--------------------------------------------------------------------------------");
         }
-        jout.println("--------------------------------------------------------------------------------");
-    }
 
-    @Override
-    public void printCourseInfomation(ArrayList<Course> courseList) {
-        jout.println("课程的基本信息：");
-        jout.println("--------------------------------------------------------------------------------------------");
-        jout.printf("%-8s\t%-8s\t%-8s\t%-8s\t%-8s\n", "序号", "课程ID", "学历", "课程名称", "价格");
-        jout.println("--------------------------------------------------------------------------------------------");
-        int number = 0;
-        for (Course cour : courseList) {
-            jout.printf("%-8d\t%-8d\t%-8s\t%-8s\t%-8.2f\n", number, cour.getCourseId(), cour.getEdu(), cour.getName(),
-                    cour.getPrice());
-            number++;
+        @Override
+        public void printCourseInfomation(ArrayList<Course> courseList) {
+                jout.println("课程的基本信息：");
+                jout.println("--------------------------------------------------------------------------------------------");
+                jout.printf("%-8s\t%-8s\t%-8s\t%-8s\t%-8s\n", "序号", "课程ID", "学历", "课程名称", "价格");
+                jout.println("--------------------------------------------------------------------------------------------");
+                int number = 0;
+                for (Course cour : courseList) {
+                        jout.printf("%-8d\t%-8d\t%-8s\t%-8s\t%-8.2f\n", number, cour.getCourseId(), cour.getEdu(),
+                                        cour.getName(), cour.getPrice());
+                        number++;
+                }
+                jout.println("--------------------------------------------------------------------------------------------");
+
         }
-        jout.println("--------------------------------------------------------------------------------------------");
 
-    }
+        @Override
+        public void printTClassInfomation(ArrayList<TClass> tClassList) {
+                jout.println("班级的基本信息：");
+                jout.println("--------------------------------------------------------------------------------------------------------");
+                jout.printf("%-8s\t%-8s\t%-8s\t%-8s\t%-8s\t%-8s\t%-8s\t%-8s\n", "序号", "班级ID", "学历", "年级", "课程名", "班号",
+                                "老师名", "学生人数");
+                jout.println("--------------------------------------------------------------------------------------------------------");
+                int number = 0;
+                for (TClass tcs : tClassList) {
+                        jout.printf("%-8d\t%-8d\t%-8s\t%-8d\t%-8s\t%-8s\t%-8d\n", number, tcs.getClassID(),
+                                        tcs.getEducation(), tcs.getGrade(), tcs.getCourseName(), tcs.getTeacherName(),
+                                        tcs.getStudentNum());
+                        number++;
+                }
+                jout.println("--------------------------------------------------------------------------------------------------------");
 
-    @Override
-    // TODO 输出班级老师
-    public void printTClassInfomation(ArrayList<TClass> tClassList) {
-        jout.println("班级的基本信息：");
-        jout.println(
-                "--------------------------------------------------------------------------------------------------------");
-        // TODO : 增加数据库查询输出课程名称
-        jout.printf("%-8s\t%-8s\t%-8s\t%-8s\t%-8s\t%-8s\t%-8s\t%-8s\n", "序号", "班级ID", "学历", "年级", "课程名", "班号", "老师名", "学生人数");
-        jout.println(
-                "--------------------------------------------------------------------------------------------------------");
-        int number = 0;
-        for (TClass tcs : tClassList) {
-            jout.printf("%-8d\t%-8d\t%-8s\t%-8d\t%-8s\t%-8s\t%-8d\n", number, tcs.getClassID(), tcs.getEducation() ,tcs.getGrade(),
-                    tcs.getCourseName(), tcs.getTeacherName(), tcs.getStudentNum());
-            number++;
         }
-        jout.println(
-                "--------------------------------------------------------------------------------------------------------");
 
-    }
-
-    @Override
-    public void printStudentAllInformation(ArrayList<Student> studentList) {
-        jout.println("学生的详细信息：");
-        jout.println(
-                "--------------------------------------------------------------------------------------------------------------");
-        jout.printf("%-8s\t%-8s\t%-8s\t%-8s\t%-8s\t%-8s\t%-8s\n", "序号", "学生ID", "姓名", "性别", "学历", "生日", "年龄");
-        jout.println(
-                "--------------------------------------------------------------------------------------------------------------");
-        int number = 0;
-        for (Student stu : studentList) {
-            jout.printf("%-8d\t%-8s\t%-8s\t%-8s\t%-8s\t%-8s\t%-8d\n", number, stu.getStudentID(), stu.getName(),
-                    stu.getSex(), stu.getEdu(), stu.getBirthday(), stu.getAge());
-            number++;
+        @Override
+        public void printStudentAllInformation(ArrayList<Student> studentList) {
+                jout.println("学生的详细信息：");
+                jout.println("--------------------------------------------------------------------------------------------------------------");
+                jout.printf("%-8s\t%-8s\t%-8s\t%-8s\t%-8s\t%-8s\t%-8s\n", "序号", "学生ID", "姓名", "性别", "学历", "生日", "年龄");
+                jout.println("--------------------------------------------------------------------------------------------------------------");
+                int number = 0;
+                for (Student stu : studentList) {
+                        jout.printf("%-8d\t%-8s\t%-8s\t%-8s\t%-8s\t%-8s\t%-8d\n", number, stu.getStudentID(),
+                                        stu.getName(), stu.getSex(), stu.getEdu(), stu.getBirthday(), stu.getAge());
+                        number++;
+                }
+                jout.println("--------------------------------------------------------------------------------------------------------------------------");
         }
-        jout.println(
-                "--------------------------------------------------------------------------------------------------------------------------");
-    }
 
-    @Override
-    public void printTeacherAllInformation(ArrayList<Teacher> teacherList) {
-        jout.println("老师的详细信息：");
-        jout.println(
-                "--------------------------------------------------------------------------------------------------------------------------");
-        jout.printf("%-8s\t%-8s\t%-8s\t%-8s\t%-8s\t%-8s\t%-8s\n", "序号", "老师ID", "姓名", "性别", "生日", "年龄", "工资");
-        jout.println(
-                "--------------------------------------------------------------------------------------------------------------------------");
-        int number = 0;
-        for (Teacher tch : teacherList) {
-            jout.printf("%-8s\t%-8s\t%-8s\t%-8s\t%-8s\t%-8d\t%-8.2f\n", number, tch.getTeacherID(), tch.getName(),
-                    tch.getSex(), tch.getBirthday(), tch.getAge(), tch.getSalary());
-            number++;
+        @Override
+        public void printTeacherAllInformation(ArrayList<Teacher> teacherList) {
+                jout.println("老师的详细信息：");
+                jout.println("--------------------------------------------------------------------------------------------------------------------------");
+                jout.printf("%-8s\t%-8s\t%-8s\t%-8s\t%-8s\t%-8s\t%-8s\n", "序号", "老师ID", "姓名", "性别", "生日", "年龄", "工资");
+                jout.println("--------------------------------------------------------------------------------------------------------------------------");
+                int number = 0;
+                for (Teacher tch : teacherList) {
+                        jout.printf("%-8s\t%-8s\t%-8s\t%-8s\t%-8s\t%-8d\t%-8.2f\n", number, tch.getTeacherID(),
+                                        tch.getName(), tch.getSex(), tch.getBirthday(), tch.getAge(), tch.getSalary());
+                        number++;
+                }
+                jout.println("--------------------------------------------------------------------------------------------------------------------------");
+
         }
-        jout.println(
-                "--------------------------------------------------------------------------------------------------------------------------");
 
-    }
+        public static void main(String[] args) {
+                PrintImpl Test = new PrintImpl();
+                // ArrayList<Student> studentList = new ArrayList<Student>();
+                // studentList.add(Debug.createStudent(001, "松本", "无", Education.HIGH,
+                // "2085-11-17", 45));
+                // studentList.add(Debug.createStudent(002, "vivy", "女", Education.PRIMARY,
+                // "2008-06-01", 34));
+                // Test.printStudentBasicInfomation(studentList);
+                // Test.printStudentAllInformation(studentList);
+                // ArrayList<Teacher> teacherList = new ArrayList<Teacher>();
+                // teacherList.add(Debug.createTeacher(001, "奥菲利亚", "女", "2045-11-17", 23,
+                // 180000.5));
+                // teacherList.add(Debug.createTeacher(002, "navy", "女", "2008-06-01", 34,
+                // 84521.0));
+                // Test.printTeacherBasicInfomation(teacherList);
+                // Test.printTeacherAllInformation(teacherList);
 
-    public static void main(String[] args) {
-        PrintImpl Test = new PrintImpl();
-        // ArrayList<Student> studentList = new ArrayList<Student>();
-        // studentList.add(Debug.createStudent(001, "松本", "无", Education.HIGH,
-        // "2085-11-17", 45));
-        // studentList.add(Debug.createStudent(002, "vivy", "女", Education.PRIMARY,
-        // "2008-06-01", 34));
-        // Test.printStudentBasicInfomation(studentList);
-        // Test.printStudentAllInformation(studentList);
-        // ArrayList<Teacher> teacherList = new ArrayList<Teacher>();
-        // teacherList.add(Debug.createTeacher(001, "奥菲利亚", "女", "2045-11-17", 23,
-        // 180000.5));
-        // teacherList.add(Debug.createTeacher(002, "navy", "女", "2008-06-01", 34,
-        // 84521.0));
-        // Test.printTeacherBasicInfomation(teacherList);
-        // Test.printTeacherAllInformation(teacherList);
+                // ArrayList<Course> courseList = new ArrayList<Course>();
+                // courseList.add(Debug.createCourse(Education.HIGH, "人工智能基础"));
+                // courseList.add(Debug.createCourse(Education.HIGH, "毛泽东思想与中国特色社会主义理论体系概论"));
+                // Test.printCourseBasicInfomation(courseList);
 
-        // ArrayList<Course> courseList = new ArrayList<Course>();
-        // courseList.add(Debug.createCourse(Education.HIGH, "人工智能基础"));
-        // courseList.add(Debug.createCourse(Education.HIGH, "毛泽东思想与中国特色社会主义理论体系概论"));
-        // Test.printCourseBasicInfomation(courseList);
-
-        ArrayList<TClass> tClassList = new ArrayList<TClass>();
-        tClassList.add(Debug.createTClass(1, 2021, Education.JUNIOR, 996, 101));
-        tClassList.add(Debug.createTClass(2, 1998, Education.PRIMARY, 007, 97));
-        Test.printTClassInfomation(tClassList);
-    }
+                ArrayList<TClass> tClassList = new ArrayList<TClass>();
+                tClassList.add(Debug.createTClass(1, 2021, Education.JUNIOR, 996, 101));
+                tClassList.add(Debug.createTClass(2, 1998, Education.PRIMARY, 007, 97));
+                Test.printTClassInfomation(tClassList);
+        }
 }
